8bf296b87e27727972efd4d6d2fe0b6986cc1c5b
arunk <arunk@192.168.156.28>
2023-11-20 17:20:32 +0530

    Selenium project

---
+++ src/test/java/mavenpkg/Fblogintest.java
@@ -0,0 +1,27 @@
+package mavenpkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Fblogintest {
+	WebDriver driver;
+	@BeforeTest
+	public void setup() {
+		driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+	}
+	@Test
+	public void test()
+	{
+		Fbloginpage ob=new Fbloginpage(driver);
+		ob.setValues("ash123@gmail.com", "abc123");
+		ob.login();
+		//driver.navigate().back();
+		//Fbcreatepage nv=new Fbcreatepage(driver);
+		//nv.pageclick();
+		//nv.getstartbutton();
+	}
+
+}
---
+++ src/test/java/mavenpkg/Saucedemotest.java
@@ -0,0 +1,5 @@
+package mavenpkg;
+
+public class Saucedemotest {
+
+}
